755486cd82b04b601255cbf13521ca54337af42f
Mischa <me@mish.dev>
2021-11-18 09:18:27 +0000

    adjusting

--- run-bot.py
+++ run-bot.py
@@ -17,7 +17,7 @@ sys.path.append(Path(__file__).parent)
 SIMULATION = False
 # how much to give the bot to play with
 # (in quote price, a.k.a. dollars)
-STARTING_BALANCE = 10000
+STARTING_BALANCE = 3000
 
 load_dotenv()
 
@@ -31,7 +31,7 @@ exchange_config = {
 if subaccount:
     exchange_config["params"]["headers"] = {"FTX-SUBACCOUNT": subaccount}
 
-db_url = os.environ.get("DATABASE_URL", "sqlite:///trader.db")
+db_url = os.environ.get("DATABASE_URL", "sqlite:///trader-v3.db")
 
 
 def gen_config(
@@ -54,9 +54,9 @@ def gen_config(
         "strategy": {"class": StratClass, "params": params},
         "timeframe": "15s",
         "entry_settings": {
-            "initial_entry_allocation": 5,
+            "initial_entry_allocation": 50,
             "signal_distance": signal_distance,  # pct under buy signal to place order
-            "leverage": 1,
+            "leverage": 2,
         },
         "exit_settings": {
             "take_profit": take_profit,
@@ -69,8 +69,8 @@ def gen_config(
 
 def params_to_bot_config(p):
     # strat params
-    fast_ma_len = p.pop("fast_ma_len")
-    slow_ma_len = p.pop("slow_ma_len")
+    fast_ma_len = p.pop("fast_ma_len", 7)
+    slow_ma_len = p.pop("slow_ma_len", 15)
     bot_config = gen_config(
         StratClass=EMACross,
         params=dict(fast_ma_len=fast_ma_len, slow_ma_len=slow_ma_len),
@@ -80,13 +80,13 @@ def params_to_bot_config(p):
 
 
 config = params_to_bot_config(
-    # return: %100.128
+    # return: %100.371
     {
-        "fast_ma_len": 4,
-        "slow_ma_len": 31,
-        "signal_distance": 0.11230040089793383,
-        "take_profit": 0.5535549416457292,
-        "stop_loss_value": 2.8214539425307237,
+        "fast_ma_len": 7,
+        "slow_ma_len": 15,
+        "signal_distance": 0.01,
+        "take_profit": 0.746652309176569,
+        "stop_loss_value": 1.189632815740987,
     }
 )
 bot_controller = defineBot(config)
